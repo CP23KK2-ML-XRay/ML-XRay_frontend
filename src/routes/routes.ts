3edd3281e9c7d6e165e67715b95c5e9baca7eeee
thanakritcankha <93530159+thanakrit2001@users.noreply.github.com>
2024-05-19 03:33:21 +0700

    UserInformation page

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,6 @@
-import SignIn from "@/views/SignIn/SignIn";
-import SignUp from "@/views/SignUp/SignUp";
-
+// import SignIn from "@/views/SignIn/SignIn";
+// import SignUp from "@/views/SignUp/SignUp";
+import UserInfo from "@/views/UserInfo/UserInfo"
 import { createBrowserRouter } from "react-router-dom";
 
 
@@ -9,7 +9,7 @@ import { createBrowserRouter } from "react-router-dom";
 export const routes = createBrowserRouter([
     {
         path: "/",
-        Component: SignUp,
+        Component: UserInfo,
         children: []
     },
 ],{ basename: "/kk2/" });
