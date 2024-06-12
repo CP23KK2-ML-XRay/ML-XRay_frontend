33e655e11aa104f76e6323a3eba3059a64654212
Thatphum <thatphum.pk@gmail.com>
2024-06-12 19:55:03 +0700

    fiex port

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5173',
+        target: 'http://localhost:3000',
         changeOrigin: true,
         ws: true,
         rewrite: (path) => path.replace(/^\/api/, '')
