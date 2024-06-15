e2d0b276975b608b9ead14a9690e5aacb5128ef6
thanakritcankha <93530159+thanakrit2001@users.noreply.github.com>
2024-06-15 21:08:28 +0700

    merch

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import { PageNotFound } from "@/views/PageNotFound/PageNotFound";
 import "./App.css";
 import UserInfo from "./views/UserInfo/UserInfo";
 import PatientsRecord from "./views/PatientsRecord/PatientsRecord";
+<<<<<<< Updated upstream
 import ModelList from "./views/ModelList/ModelList";
 import SignIn from "./views/SignIn/SignIn";
+=======
+>>>>>>> Stashed changes
 
 function App() {
   return (
@@ -13,8 +16,12 @@ function App() {
         <Route path="*" element={<PageNotFound />} />
         <Route path="/" element={<UserInfo />} />
         <Route path="/patients" element={<PatientsRecord />} />
+<<<<<<< Updated upstream
         <Route path="/about" element={<ModelList />} />
         <Route path="/signin" element={<SignIn />} />
+=======
+        <Route path="/about" element={<About />} />
+>>>>>>> Stashed changes
       </Routes>
     </BrowserRouter>
   );
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,16 +1,25 @@
 // import SignIn from "@/views/SignIn/SignIn";
 // import SignUp from "@/views/SignUp/SignUp";
 // import UserInfo from "@/views/UserInfo/UserInfo"
+<<<<<<< Updated upstream
 import PatientsRecord from '@/views/PatientsRecord/PatientsRecord'
 import PatientDetail from '@/views/PatientsRecord/PatientDetail'
 import ModelList from '@/views/ModelList/ModelList'
 import { Layout } from '@/components/_Layout'
 import { createBrowserRouter } from 'react-router-dom'
 import SignIn from '@/views/SignIn/SignIn'
+=======
+import PatientsRecord from "@/views/PatientsRecord/PatientsRecord";
+import PatientDetail from "@/views/PatientsRecord/PatientDetail";
+import ModelList from "@/views/ModelList/ModelList";
+import { Layout } from "@/components/_Layout";
+import { createBrowserRouter } from "react-router-dom";
+>>>>>>> Stashed changes
 
 export const routes = createBrowserRouter(
   [
     {
+<<<<<<< Updated upstream
       path: '/',
       Component: Layout,
       children: [],
@@ -33,3 +42,15 @@ export const routes = createBrowserRouter(
   ],
   { basename: '/kk2/' }
 )
+=======
+        path: "/",
+        Component: Layout,
+        children: []
+    },
+    {
+        path: "/patients",
+        Component: PatientsRecord,
+        children: []
+    }
+],{ basename: "/kk2/" });
+>>>>>>> Stashed changes
