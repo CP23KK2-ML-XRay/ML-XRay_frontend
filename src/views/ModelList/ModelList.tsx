3f6e2c88a65ec6532408ff28422f48c422f0496a
thanakritcankha <93530159+thanakrit2001@users.noreply.github.com>
2024-06-17 09:59:41 +0700

    model list

--- src/views/ModelList/ModelList.tsx
+++ src/views/ModelList/ModelList.tsx
@@ -1,4 +1,36 @@
+import React, { useState } from 'react'
+
 const ModelList = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
+  const [isEditOpen, setIsEditOpen] = useState(false)
+
+  const openEdit = () => {
+    setIsEditOpen(true)
+  }
+
+  const closeEdit = () => {
+    setIsEditOpen(false)
+  }
+
+  const [isDeleteOpen, setIsDeleteOpen] = useState(false)
+
+  const openDelete = () => {
+    setIsDeleteOpen(true)
+  }
+
+  const closeDelete = () => {
+    setIsDeleteOpen(false)
+  }
+
   return (
     <section className="h-screen w-screen bg-gray-50 dark:bg-gray-900 p-3 sm:p-5">
       <div className="mx-auto max-w-screen-xl px-4 lg:px-12">
@@ -38,6 +70,7 @@ const ModelList = () => {
               <button
                 type="button"
                 className="flex items-center justify-center bg-blue-600 text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+                onClick={openModal}
               >
                 <svg
                   className="h-3.5 w-3.5 mr-2"
@@ -93,13 +126,14 @@ const ModelList = () => {
                   <td className="px-4 py-3">Image</td>
                   <td className="px-4 py-3">Dogs</td>
                   <td className="px-4 py-3">Cats</td>
-                  <td className="px-4 py-3">Pigs</td>
+                  <td className="px-4 py-3">UNKNOW</td>
                   <td className="pr-4 py-3 flex items-center justify-center">
                     <button
                       id="apple-imac-27-dropdown-button"
                       data-dropdown-toggle="apple-imac-27-dropdown"
                       className="inline-flex items-center mx-2 p-0.5 text-sm font-medium text-center bg-blue-600 text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
                       type="button"
+                      onClick={openEdit}
                     >
                       <svg
                         className="w-5 h-5 text-gray-800 dark:text-white"
@@ -125,6 +159,7 @@ const ModelList = () => {
                       data-dropdown-toggle="apple-imac-27-dropdown"
                       className="inline-flex items-center mx-2 p-0.5 text-sm font-medium text-center bg-red-600 text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
                       type="button"
+                      onClick={openDelete}
                     >
                       <svg
                         className="w-5 h-5 text-gray-800 dark:text-white"
@@ -251,8 +286,249 @@ const ModelList = () => {
           </nav>
         </div>
       </div>
-    </section>
-  );
-};
 
-export default ModelList;
+      {/*Modal for EDIT MODEL _-----------------------------------------------------------------------------------*/}
+      {isEditOpen && (
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div
+              className="fixed inset-0 transition-opacity"
+              aria-hidden="true"
+            >
+              <div className="absolute inset-0 bg-gray-400 opacity-25"></div>
+            </div>
+
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <div className="inline-block align-bottom bg-white dark:bg-gray-800 rounded-lg text-left overflow-hidden shadow-2xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              {/* Header */}
+              <div className="bg-primary-500 px-4 py-3 sm:px-6">
+                <h3 className="text-2xl font-medium leading-6 text-black">
+                  Edit Model Name
+                </h3>
+              </div>
+
+              {/* Modal body */}
+              <form className="space-y-4 md:space-y-6">
+                <div className="px-4 py-4 sm:p-6">
+                  <div className="grid grid-cols-1 gap-6 w-full">
+                    <label
+                      htmlFor="modelName"
+                      className="text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Model Name
+                    </label>
+                    <input
+                      type="text"
+                      id="modelName"
+                      name="modelName"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      required
+                    />
+                  </div>
+                </div>
+
+                {/* Modal footer */}
+                <div className="bg-white dark:bg-gray-400 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                  <button
+                    type="button"
+                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-slate-600 text-base font-medium text-white hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={closeEdit}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    type="submit"
+                    className="w-1/3 text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/*Modal for Create new model ------------------------------------------------------------------------------------------------ */}
+      {isModalOpen && (
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div
+              className="fixed inset-0 transition-opacity"
+              aria-hidden="true"
+            >
+              <div className="absolute inset-0 bg-gray-400 opacity-25"></div>
+            </div>
+
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <div className="inline-block align-bottom bg-white dark:bg-gray-800 rounded-lg text-left overflow-hidden shadow-2xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              {/* Header */}
+              <div className="bg-primary-500 px-4 py-3 sm:px-6">
+                <h3 className="text-2xl font-medium leading-6 text-black">
+                  Create Model
+                </h3>
+              </div>
+
+              {/* Modal body */}
+              <form action="" className="space-y-4 md:space-y-6">
+                <div className="px-4 py-4 sm:p-6">
+                  <div className="grid grid-cols-1 gap-6 w-full">
+                    <label
+                      htmlFor="name"
+                      className="text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Model Name
+                    </label>
+                    <input
+                      type="text"
+                      id="name"
+                      name="name"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    />
+                    <label
+                      htmlFor="category"
+                      className="text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Model Category
+                    </label>
+                    <select
+                      id="category"
+                      name="category"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    >
+                      <option value="" selected disabled hidden>
+                        Choose here
+                      </option>
+                      <option value="image">Images classifying model</option>
+                      <option value="numeric">
+                        Numeric and categorical features model
+                      </option>
+                    </select>
+                    <label
+                      htmlFor="class0"
+                      className="text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Identify class 0:
+                    </label>
+                    <input
+                      type="text"
+                      id="class0"
+                      name="class0"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    />
+                    <label
+                      htmlFor="class1"
+                      className="text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Identify class 1:
+                    </label>
+                    <input
+                      type="text"
+                      id="class1"
+                      name="class1"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    />
+                    <label
+                      htmlFor="class2"
+                      className="text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Identify class 2:
+                    </label>
+                    <input
+                      type="text"
+                      id="class2"
+                      name="class2"
+                      className="bg-white-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    />
+                  </div>
+                </div>
+
+                {/* Modal footer */}
+                <div className="bg-white dark:bg-gray-400 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                  <button
+                    type="button"
+                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-primary-600 text-base font-medium text-black hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={closeModal}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    type="submit"
+                    className="w-1/3 text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/*Modal for delete-----------------------------------------------------------------------------------------------------------------------*/}
+      {isDeleteOpen && (
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div
+              className="fixed inset-0 transition-opacity"
+              aria-hidden="true"
+            >
+              <div className="absolute inset-0 bg-gray-400 opacity-25"></div>
+            </div>
+
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <div className="inline-block align-bottom bg-white dark:bg-gray-800 rounded-lg text-left overflow-hidden shadow-2xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              {/* Header */}
+              <div className="bg-primary-500 px-4 py-3 sm:px-6">
+                <h3 className="text-2xl font-medium leading-6 text-black">
+                  Confirm Deletion
+                </h3>
+              </div>
+
+              {/* Modal body */}
+              <div className="px-4 py-4 sm:p-6">
+                <p className="text-sm font-medium text-gray-900 dark:text-white">
+                  Are you sure you want to delete this model?
+                </p>
+              </div>
+
+              {/* Modal footer */}
+              <div className="bg-white dark:bg-gray-400 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <button
+                  type="button"
+                  className="w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                  onClick={closeDelete}
+                >
+                  No
+                </button>
+                <button
+                  type="button"
+                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:mt-0 sm:w-auto sm:text-sm"
+                >
+                  Yes
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </section>
+  )
+}
+
+export default ModelList
