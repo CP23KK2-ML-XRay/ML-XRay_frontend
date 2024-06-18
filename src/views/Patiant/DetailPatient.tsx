cbe134ec4b179dad7a2fa99239983b7956d3d9a2
thanakritcankha <93530159+thanakrit2001@users.noreply.github.com>
2024-06-19 03:45:14 +0700

    Update DetailPatient.tsx

--- src/views/Patiant/DetailPatient.tsx
+++ src/views/Patiant/DetailPatient.tsx
@@ -457,9 +457,11 @@ export const DetailPatient = () => {
               <select
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 id="modelName"
-                name='modelName'
+                name="modelName"
               >
-                <option value="" selected disabled>Select Model</option>
+                <option value="" selected disabled>
+                  Select Model
+                </option>
                 <option value="CA">2</option>
                 <option value="FR">3</option>
                 <option value="DE">4</option>
