65988fa5e33c5a9e4d1bd0d6340146e725c84407
Preeyathorn Chaiyakum <neko7687@gmail.com>
2024-06-19 01:40:16 +0700

    Signup UI

--- src/views/SignUp/SignUp.tsx
+++ src/views/SignUp/SignUp.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { useNavigate, Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Swal from "sweetalert2";
 
 const SignUp = () => {
